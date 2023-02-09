e38bd13b82828bac1e02c4b55c784b4e0131ca73
Stepan Kabanov <phoenixgenera@gmail.com>
2023-02-09 22:13:33 +0300

    implemented road recording

--- check_road.py
+++ check_road.py
@@ -5,19 +5,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from scipy import interpolate
 def dist(x, y):
     x = np.array(x)
     y = np.array(y)
     return np.norm(y-x)
 
+# def extend(xs): 
+#     # extends x in the following sense (only for integers):
+#     # [1,2,5] -> [1,2,3,4,5]
+#     # [-1,2,3,6,5,4,1,-1] -> [-1,0,1,2,3,4,5,6,5,4,3,2,1,0,-1]
+#     extended = []
+#     m = xs[0]
+#     for x in xs:
+#         if x == m:
+#             extended.append(x)
+#             continue
+#         while x >= m + 1:
+#             m+=1
+#             extended.append(m)
+#         while x < m:
+#             m-=1
+#             extended.append(m)
+#         m = x
+#     return extended
+
 list_of_files = glob.glob('C:\\Users\\stepa\\Documents\\AirSim\\*')
 latest_record = max(list_of_files, key=os.path.getctime) + '\\airsim_rec.txt'
 
 route_record = pd.read_csv(latest_record, sep='\t')[['POS_X','POS_Y']].rename(columns={'POS_X':'x', 'POS_Y':'y'})
-route_record = route_record.round(1).drop_duplicates()
-route_record['prev_x'] = route_record['x'].shift(1)
-route_record['prev_y'] = route_record['y'].shift(1)
-route_record = route_record.drop(0)
+route_record = route_record.round(2).drop_duplicates()
+
+# psint(new_record)
+print(route_record)
+
+# """
+# route_record['y'] = np.interp(np.linspace(-5,80,100),route_record['x'],route_record['y'])
+# route_record['x'] = np.linspace()
+# route_record['prev_x'] = route_record['x'].shift(1)
+# route_record['prev_y'] = route_record['y'].shift(1)
+# route_record = route_record.drop(0)
 line_width = 3
 # route_record['left_road_line'] = ...
 # route_record['right_road_line'] = ...
@@ -58,10 +85,8 @@ def calc_road_lines(a, b,h=line_width):
     return (xl, yl), (xr, yr)
 
 
-def test_lanes(x,y,h=3):
+def get_lanes(x,y,h=3,show=False):
     dots = list(zip(x,y))
-
-
     road_left = []
     road_right = []
     for i in range(1,len(dots)):
@@ -76,81 +101,57 @@ def test_lanes(x,y,h=3):
 
     xl = get_line(road_left,0)
     yl = get_line(road_left,1)
+    if show:
+        x_space = np.array(np.arange(-5,80,1))
+        y_space = np.array(np.arange(5,-60,-1))
+        xv, yv = np.meshgrid(x_space, y_space)
 
-    x_space = np.linspace(-5,80,50).round(1)
-    y_space = np.linspace(5,-60,50).round(1)
-
-    xv, yv = np.meshgrid(x_space, y_space)
-
-    def check_if_inside(x, y,h=3):
-        
-        # many cases
-        
-        y_xl = yl[np.where(xl == x)]
-        y_xl_up = y_xl[y_xl > y]
-        y_xl_down = y_xl[y_xl < y]
-
-        y_xr = yr[np.where(xr == x)]
-        y_xr_up = y_xr[y_xr > y]
-        y_xr_down = y_xr[y_xr < y]
-        # moving left?
-        # if all arrays are exist
-        adv_min = lambda x: x.min() if x.any() else 10000000
-        adv_max = lambda x: x.max() if x.any() else -10000000 
-        
-        debug = lambda i: print(x,y,'case', i)
-        if adv_min(y_xl_up) < adv_min(y_xr_up) and adv_max(y_xr_down) > adv_max(y_xl_down):
-            return True
-        # moving right?
-        elif adv_min(y_xl_up) > adv_min(y_xr_up) and adv_max(y_xr_down) < adv_max(y_xl_down):
-            return True
-
-        x_yl = xl[np.where(yl == y)]
-        x_yl_right = x_yl[x_yl > x]
-        x_yl_left = x_yl[x_yl < x]
-
-        x_yr = xr[np.where(yr == y)]
-        x_yr_right = x_yr[x_yr > x]
-        x_yr_left = x_yr[x_yr < x]
-        # moving up
-        if adv_min(x_yr_right) < adv_min(x_yl_right) and adv_max(x_yl_left) > adv_max(x_yr_left):
-            return True
-        # moving down?
-        elif adv_min(x_yr_right) > adv_min(x_yl_right) and adv_max(x_yl_left) < adv_max(x_yr_left):
-            return True
-        
-        return False
-    
-    xv_out = []
-    yv_out = []
-    xv_in = []
-    yv_in = []
-
-    for x__ in xv:
-        for y__ in yv:
-            for x_, y_ in list(zip(x__,y__)):
-                if check_if_inside(x_, y_):
-                    xv_in.append(x_)
-                    yv_in.append(y_)
-                else:
-                    xv_out.append(x_)
-                    yv_out.append(y_)            
-
-    plt.scatter(xv_out, yv_out,color='yellow')
-    plt.scatter(xv_in, yv_in,color='red')
-
-    plt.plot(xr,yr,'.-',label='right_lane')
-    plt.plot(xl,yl,'.-',label='left_lane')
-    plt.plot(x,y,'.-',label='center')
-    plt.legend()
-    plt.show()
+        plt.plot(xr,yr,'.-',label='right_lane')
+        plt.plot(xl,yl,'.-',label='left_lane')
+        plt.plot(x,y,'.-',label='center')
+        plt.legend()
+        plt.show()
     return xl,yl,xr,yr
 
-### test sets of road_centers
 
 x = np.array(route_record['x'])
 y = np.array(route_record['y'])
 
-xl,yl,xr,yr = test_lanes(x,y)
+xl,yl,xr,yr = get_lanes(x,y, show=False)
+x_upper_limit = max(max(xl), max(xr)) + 5
+x_lower_limit = min(min(xl), min(xr)) - 5
+y_upper_limit = max(max(yl), max(yr)) + 5
+y_lower_limit = min(min(yl), min(yr)) - 5
+
+x_space = np.linspace(x_lower_limit, x_upper_limit, 50)
+y_space = np.linspace(y_lower_limit, y_upper_limit, 50)
+xv, yv = np.meshgrid(x_space, y_space)
 
 
+from matplotlib import path
+import matplotlib.patches as patches
+p = path.Path(list(zip(xl, yl))+list(zip(xr,yr))[::-1])
+
+
+xv_in, yv_in = [], []
+xv_out, yv_out = [], []
+for x_, y_ in zip(xv, yv):
+    for x, y in zip(x_, y_):
+        if p.contains_points([(x,y)]):
+            xv_in.append(x)
+            yv_in.append(y)
+        else:
+            xv_out.append(x)
+            yv_out.append(y)
+
+fig, ax = plt.subplots()
+patch = patches.PathPatch(p,color=None)
+ax.add_patch(patch)
+ax.scatter(xv_in, yv_in,color='red')
+ax.scatter(xv_out, yv_out,color='yellow')
+
+ax.set_xlim(x_lower_limit, x_upper_limit)
+ax.set_ylim(y_lower_limit, y_upper_limit)
+# print(p.contains_points([(125,-45)]))
+plt.show()
+# """
--- record_road.py
+++ record_road.py
@@ -2,36 +2,133 @@
 # The Road generation can be implemented in 2 ways
 # -------------------------------------------
 # First approach
-# A user itself controls the car and his trajectory is recorded. His 
-# trajectory is used to define the road. 
+# A user itself controls the car and his trajectory is recorded. His
+# trajectory is used to define the road.
 # -------------------------------------------
 # Second approach
 # A road can be generated using the random road generator with the given parameters
 # of curvity and length of the road.
-# 
+#
 # Here is an implementation of the first approach
 if __name__ == '__main__':
+
     import utils
+    import time
     import keyboard
     import airsim
+    import glob
+    import os
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from datetime import datetime
 
     simulator = utils.Simulator()
-    
-    print("This is a road generator. \nPress the key `u` to start. \nPress the key `r` to start recording\nPress the key `u` again to finish the ride ")
 
+    print("This is a road generator.")
+    print("Do you want to record train or test road?")
+
+    test = bool(int(input("Type [0] for train and [1] for test road: ")))
+    save_path = 'roads\\test\\' if test else 'roads\\train\\'
+
+    print("Press the key `u` to start (recording will start automaticly). \nPress the key `u` again to finish the ride")
     while not keyboard.is_pressed('u'):
         pass
 
     simulator.start_simulator()
-    
     print("Starting the simulator ...")
-
-    while not keyboard.is_pressed('r'):
-        pass
-    
+    time.sleep(3)
     car = airsim.client.CarClient(ip='127.0.0.1')
+    car.startRecording()
+    print("Recording has started ...")
 
     while not keyboard.is_pressed('u'):
         pass
     car.stopRecording()
-    
+    print("Recording is finished")
+
+    list_of_files = glob.glob('C:\\Users\\stepa\\Documents\\AirSim\\*')
+    latest_record = max(
+        list_of_files, key=os.path.getctime) + '\\airsim_rec.txt'
+
+    route_record = pd.read_csv(latest_record, sep='\t')[['POS_X', 'POS_Y']].rename(
+        columns={'POS_X': 'x', 'POS_Y': 'y'})
+    route_record = route_record.round(2).drop_duplicates()
+
+    line_width = 3
+
+    def calc_road_lines(a, b, h=line_width):
+        x1, y1 = a
+        x2, y2 = b
+
+        alpha = np.arctan(abs(y2-y1)/abs(x2-x1))
+        if x2 - x1 > 0:
+            if y2 - y1 > 0:
+                xl = x2 - (h/2)*np.sin(alpha)
+                yl = y2 + (h/2)*np.cos(alpha)
+
+                xr = x2 + (h/2)*np.sin(alpha)
+                yr = y2 - (h/2)*np.cos(alpha)
+            else:
+                xl = x2 + (h/2)*np.sin(alpha)
+                yl = y2 + (h/2)*np.cos(alpha)
+
+                xr = x2 - (h/2)*np.sin(alpha)
+                yr = y2 - (h/2)*np.cos(alpha)
+        else:
+            if y2 - y1 > 0:
+                xl = x2 - (h/2)*np.sin(alpha)
+                yl = y2 - (h/2)*np.cos(alpha)
+
+                xr = x2 + (h/2)*np.sin(alpha)
+                yr = y2 + (h/2)*np.cos(alpha)
+            else:
+                xl = x2 + (h/2)*np.sin(alpha)
+                yl = y2 - (h/2)*np.cos(alpha)
+
+                xr = x2 - (h/2)*np.sin(alpha)
+                yr = y2 + (h/2)*np.cos(alpha)
+
+        return (xl, yl), (xr, yr)
+
+    def get_lanes(x, y, h=3, show=False):
+        dots = list(zip(x, y))
+        road_left = []
+        road_right = []
+        for i in range(1, len(dots)):
+            left, right = calc_road_lines(dots[i-1], dots[i], h=h)
+            road_left.append(left)
+            road_right.append(right)
+
+        def get_line(road_line, i): return np.array(
+            list(map(lambda x: x[i], road_line)))
+
+        xr = get_line(road_right, 0)
+        yr = get_line(road_right, 1)
+
+        xl = get_line(road_left, 0)
+        yl = get_line(road_left, 1)
+        if show:
+            x_space = np.array(np.arange(-5, 80, 1))
+            y_space = np.array(np.arange(5, -60, -1))
+            xv, yv = np.meshgrid(x_space, y_space)
+
+            plt.plot(xr, yr, '.-', label='right_lane')
+            plt.plot(xl, yl, '.-', label='left_lane')
+            plt.plot(x, y, '.-', label='center')
+            plt.legend()
+            plt.show()
+        return xl, yl, xr, yr
+    x, y = np.array(route_record['x']), np.array(route_record['y'])
+    xl, yl, xr, yr = get_lanes(x, y, show=True)
+    print('Do you agree to record this road?')
+    confirmation = bool(int(input("Type [0] for delete record and [1] for save it: ")))
+    if confirmation:
+        df = pd.DataFrame()
+        df['xl'] = xl
+        df['yl'] = yl
+        df['xr'] = xr
+        df['yr'] = yr
+        save_date = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        print(save_date)
+        df.to_csv(save_path+'\\'+save_date+'.csv')
